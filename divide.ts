1b3b8ae5d58ff7d1ce319391a26543c98860cd0d
AliyanShaikh66 <aliyanzahid02@gmail.com>
2024-05-09 19:44:53 +0500

    Calculator

---
+++ divide.ts
@@ -0,0 +1,17 @@
+let num1 = prompt('Enter first number');
+
+console.log (num1);
+
+let num2 = prompt('Enter second number');
+
+console.log (num2);
+
+num1 = Number(num1);
+
+num2 = Number(num2);
+
+let answer = (num1 / num2);
+
+alert ('The ratio is : ' + answer);
+
+console.log ('The ratio is : ' + answer);
